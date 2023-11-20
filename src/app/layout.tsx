0690a2580496815d259162b89874a5208b5f56fe
Beata Gonera <beataiwonagonera@gmail.com>
2023-11-20 20:25:33 +0100

    suspense with loading animation added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inria_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/ui/organisms/Navbar";
 import MenuMobile from "@/ui/organisms/MenuMobile";
-import LoadingPage from "@/ui/atoms/LoadingPage";
+import LoadingPage from "@/ui/atoms/LoadingTea";
+import { Suspense } from "react";
 
 const inria = Inria_Sans({
   weight: ["300", "400", "700"],
@@ -23,12 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inria.className}>
-        <Navbar />
-        <MenuMobile />
-        {/* <LoadingPage /> */}
-        {children}
-      </body>
+      <Suspense fallback={<LoadingPage />}>
+        <body className={inria.className}>
+          <Navbar />
+          <MenuMobile />
+          {children}
+        </body>
+      </Suspense>
     </html>
   );
 }
---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+import LoadingTea from "@/ui/atoms/LoadingTea";
+const LoadingPage = () => {
+  return <LoadingTea />;
+};
+
+export default LoadingPage;
